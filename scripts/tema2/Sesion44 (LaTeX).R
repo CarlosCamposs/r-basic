c342c21a6646c75d62321780e449ae105c92da1c
CarlosCamposs <carlosdanct7@gmail.com>
2022-07-30 15:09:18 -0500

    .

---
+++ scripts/tema2/Sesion44 (LaTeX).R
@@ -0,0 +1,71 @@
+# Sesion 44
+
+# En LaTeX se pueden escribir las matrices de diversas formas:
+
+# \begin{matrix}
+#   _____   
+# \end{matrix}
+
+
+# Para una matriz con paréntesis
+# \begin{pmatrix}
+#   _____   
+# \end{pmatrix}
+
+
+# Para una matriz con barras de determinante (vertical matrix)
+# \begin{vmatrix}
+#   _____   
+# \end{vmatrix}
+
+
+# Para una matriz con barras de una norma (vector) (vertical matrix)
+# \begin{Vmatrix}
+#   _____   
+# \end{Vmatrix}
+
+
+# Para una matriz con barras de corchete (no llaves)
+# \begin{bmatrix}
+#   _____   
+# \end{bmatrix}
+
+
+# Para una matriz con llaves (se pone una "B")
+# \begin{Bmatrix}
+#   _____   
+# \end{Bmatrix}
+
+
+#Sistemas de ecuaciones
+
+
+# \begin{array}{\l}
+#   _____   
+# \end{array}
+    # Se dice que {l} es un modificador, nos dice hacia donde se pondrá el texto (alineación)
+
+
+
+# \left. \right. hace que el delimitador respectivo no aparezca
+
+
+## $$\left. \begin{array}{l} ax+by=c\\ ex-fy=g  \end{array} \right \}$$
+
+    #Lo que se hace es un ARRAY, por lo que si queremos una "}" se debe especificar con "\}"
+
+
+# Para el símbolo <=  se usa "\le"  (less or equal)
+# Para el símbolo >= se usa "\ge"   (great or equal)
+
+
+
+
+
+
+
+
+
+
+
+
